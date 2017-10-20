8ddb09e6b38df37513ca637f4585b5bee06efbce
Jang Zhang <jangz@synnex.com>
2017-10-20 14:15:51 +0800

    细化验证3个结论

---
+++ src/zychaowill/discover/smallskill/multi/Run.java
@@ -0,0 +1,43 @@
+package zychaowill.discover.smallskill.multi;
+
+public class Run {
+	public static void main(String[] args) {
+		MyList list = new MyList();
+		MyThreadA ta = new MyThreadA(list);
+		ta.setName("A");
+		ta.start();
+		MyThreadB tb = new MyThreadB(list);
+		tb.setName("B");
+		tb.start();
+	}
+}
+
+class MyThreadA extends Thread {
+	private MyList list;
+
+	public MyThreadA(MyList list) {
+		this.list = list;
+	}
+	
+	@Override
+	public void run() {
+		for (int i = 0; i < 100000; i++) {
+			list.add("threadA" + (i + 1));
+		}
+	}
+}
+
+class MyThreadB extends Thread {
+	private MyList list;
+
+	public MyThreadB(MyList list) {
+		this.list = list;
+	}
+	
+	@Override
+	public void run() {
+		for (int i = 0; i < 100000; i++) {
+			list.add("threadB" + (i + 1));
+		}
+	}
+}
