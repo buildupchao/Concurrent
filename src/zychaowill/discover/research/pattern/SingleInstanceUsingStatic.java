6b34e70432acd1aed880dd62e2e8192cd0a689e5
Jang Zhang <jangz@synnex.com>
2018-02-06 13:53:30 +0800

    Single instance.

---
+++ src/zychaowill/discover/research/pattern/SingleInstanceUsingStatic.java
@@ -0,0 +1,16 @@
+package zychaowill.discover.research.pattern;
+
+public class SingleInstanceUsingStatic {
+
+	private static SingleInstanceUsingStatic instance = null;
+
+	static {
+		instance = new SingleInstanceUsingStatic();
+	}
+
+	private SingleInstanceUsingStatic() {}
+
+	public static SingleInstanceUsingStatic getInstance() {
+		return instance;
+	}
+}
