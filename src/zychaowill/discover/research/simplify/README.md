16323376cad2f818580fd060b27fb90ef5fd79ce
Jang Zhang <jangz@synnex.com>
2018-02-06 12:23:52 +0800

    ReentrantLock and ReentrantReadWriteLock.

--- src/zychaowill/discover/research/simplify/README.md
+++ src/zychaowill/discover/research/simplify/README.md
@@ -1,5 +1,6 @@
-Run ReentrantLockExpression to get result as follows:
+- Run ReentrantLockExpression to get result as follows:
 
+```
 [tryReentrantLock] begin to execute, name is A1, time is 1517880806747.
 [tryReentrantLock] end to execute, name is A1, time is 1517880811754.
 [tryReentrantLockAgain] begin to execute, name is B1, time is 1517880811754.
@@ -8,3 +9,15 @@ Run ReentrantLockExpression to get result as follows:
 [tryReentrantLockAgain] end to execute, name is B2, time is 1517880821756.
 [tryReentrantLock] begin to execute, name is A2, time is 1517880821756.
 [tryReentrantLock] end to execute, name is A2, time is 1517880826757.
+```
+
+- Run ReentrantReadWriteLockExpressionExample to get result as follows:
+
+> 读写、写读和写写都是互斥的；而读读是异步的，非互斥的。
+
+```
+2018-02-06 12:21:09,864  INFO [A] (ReentrantReadWriteLockExpression.java:28) - [write] A holds write-lock at 1517890869863.
+2018-02-06 12:21:12,866  INFO [A] (ReentrantReadWriteLockExpression.java:33) - [write] A releases write-lock at 1517890872866.
+2018-02-06 12:21:12,867  INFO [B] (ReentrantReadWriteLockExpression.java:15) - [read] B holds read-lock at 1517890872867.
+2018-02-06 12:21:15,868  INFO [B] (ReentrantReadWriteLockExpression.java:20) - [read] B releases read-lock at 1517890875868.
+```
---
+++ src/zychaowill/discover/research/simplify/ReentrantReadWriteLockExpressionExample.java
@@ -0,0 +1,57 @@
+package zychaowill.discover.research.simplify;
+
+public class ReentrantReadWriteLockExpressionExample {
+	
+	public static void main(String[] args) {
+//		readLockSharing();
+//		writeLockMutex();
+//		readWriteMutex();
+		writeReadMutex();
+	}
+	
+	private static void readLockSharing() {
+		final ReentrantReadWriteLockExpression service = new ReentrantReadWriteLockExpression();
+		Runnable target = () -> service.read();
+		
+		Thread a = new Thread(target);
+		a.setName("A");
+		Thread b = new Thread(target);
+		b.setName("B");
+		a.start();
+		b.start();
+	}
+	
+	private static void writeLockMutex() {
+		final ReentrantReadWriteLockExpression service = new ReentrantReadWriteLockExpression();
+		Runnable target = () -> service.write();
+		
+		Thread a = new Thread(target);
+		a.setName("A");
+		Thread b = new Thread(target);
+		b.setName("B");
+		a.start();
+		b.start();
+	}
+	
+	private static void readWriteMutex() {
+		final ReentrantReadWriteLockExpression service = new ReentrantReadWriteLockExpression();
+		
+		Thread a = new Thread(() -> service.read());
+		a.setName("A");
+		Thread b = new Thread(() -> service.write());
+		b.setName("B");
+		a.start();
+		b.start();
+	}
+	
+	private static void writeReadMutex() {
+		final ReentrantReadWriteLockExpression service = new ReentrantReadWriteLockExpression();
+		
+		Thread a = new Thread(() -> service.write());
+		a.setName("A");
+		Thread b = new Thread(() -> service.read());
+		b.setName("B");
+		a.start();
+		b.start();
+	}
+}
