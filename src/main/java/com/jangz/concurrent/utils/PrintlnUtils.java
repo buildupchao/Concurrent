c74ac051c7ee4f209ba0e181f44b9f7cda9cace3
Jang Zhang <jangz@synnex.com>
2018-04-25 13:36:39 +0800

    unsafe HashMap

--- src/main/java/com/jangz/concurrent/utils/PrintlnUtils.java
+++ src/main/java/com/jangz/concurrent/utils/PrintlnUtils.java
@@ -6,9 +6,12 @@ import java.io.OutputStream;
 
 public class PrintlnUtils {
 
+	private static final String BASE = System.getProperty("user.dir");
+	private static final String PATH = "src/main/resources/_result";
+	
 	public static void println(Object o) {
 		try {
-			OutputStream out = new FileOutputStream(new File("D:\\jangz\\git\\orders.log"), true);
+			OutputStream out = new FileOutputStream(new File(BASE + "/" + PATH), true);
 			out.write((o.toString() + "\n").getBytes());
 			out.close();
 		} catch (Exception e) {
