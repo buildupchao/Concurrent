435c55f287d8a4b1697d617f01bbc38bd31f4e46
Zychaowill <15536542139@163.com>
2019-03-17 19:38:36 +0800

    provide socket net examples

---
+++ src/main/java/com/jangz/concurrent/discover/research/action/net/Constants.java
@@ -0,0 +1,12 @@
+package com.jangz.concurrent.discover.research.action.net;
+
+/**
+ * @author buildupchao
+ *         Date: 2019/3/17 19:23
+ * @since JDK 1.8
+ */
+public class Constants {
+
+    public static final String host = "localhost";
+    public static final int port = 8888;
+}
---
+++ src/main/java/com/jangz/concurrent/discover/research/action/net/ServerAcceptor.java
@@ -0,0 +1,70 @@
+package com.jangz.concurrent.discover.research.action.net;
+
+import com.google.common.collect.Queues;
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author buildupchao
+ *         Date: 2019/3/17 19:07
+ * @since JDK 1.8
+ */
+public class ServerAcceptor implements Runnable {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServerAcceptor.class);
+
+    private static final ExecutorService SOCKET_SUBMIT_POOL = new ThreadPoolExecutor(
+      50,
+            100,
+            0,
+            TimeUnit.SECONDS,
+            Queues.newArrayBlockingQueue(10),
+            new BasicThreadFactory.Builder().namingPattern("socket-submit-%d").build(),
+            new ThreadPoolExecutor.AbortPolicy()
+    );
+    private int port;
+    private volatile boolean stop = false;
+
+    public ServerAcceptor(int port) {
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        try {
+            ServerSocket acceptor = new ServerSocket(port);
+            LOGGER.info("server acceptor starts, listens to port=[{}]", port);
+
+//            int i = 0;
+            while (true) {
+//                if (i == 5) {
+//                    Thread.sleep(5000);
+//                    throw new RuntimeException("generate an error!");
+//                }
+
+                final Socket socket = acceptor.accept();
+
+                SOCKET_SUBMIT_POOL.execute(() -> {
+                    SocketManager.pushSocket(socket);
+                });
+//                i++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            LOGGER.info("server acceptor exits!");
+        }
+    }
+
+    public void setStop(boolean stop) {
+        this.stop = stop;
+    }
+}
